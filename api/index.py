bf1e382103bfb6a929466baba4439086a0818035
Yogesh <93325653+Yoge-sh@users.noreply.github.com>
2024-05-07 15:44:36 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,26 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['GET','POST'])
 def home():
-    if request.method == 'POST':
-        user_input = request.get_json()    
-        name, age, sex,location, foodpref, wc, activity, history = user_input.values()
+    if request.method == 'GET':
+        return "Hello G!"
+    # if request.method == 'POST':
+    #     user_input = request.get_json()    
+    #     name, age, sex,location, foodpref, wc, activity, history = user_input.values()
         
-        results = {
-            "Patient Name" : name,
-            "Age": age,
-            "Sex": sex,
-            "Location" : location,
-            "Food Preference" : foodpref,
-            "Waist Circumference": wc,
-            "Activity Level": 2,
-            "Diabetes History": 2,
-            "Total Score": 30,
-            "Prediction": "high risk"
-        }
-        return results
-    return 'This endpoint expects a POST request with user input data in JSON format.'
+    #     results = {
+    #         "Patient Name" : name,
+    #         "Age": age,
+    #         "Sex": sex,
+    #         "Location" : location,
+    #         "Food Preference" : foodpref,
+    #         "Waist Circumference": wc,
+    #         "Activity Level": 2,
+    #         "Diabetes History": 2,
+    #         "Total Score": 30,
+    #         "Prediction": "high risk"
+    #     }
+    #     return results
+    return 'This endpoint expects a GET/POST request with user input data in JSON format.'
 
 @app.route('/about')
 def about():
