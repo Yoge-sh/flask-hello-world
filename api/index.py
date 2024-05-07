b76966ed0da41838791e3dd280b9868c6a764db7
Yogesh <93325653+Yoge-sh@users.noreply.github.com>
2024-05-07 15:17:36 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World! From hhHHee'
+    name, age, sex,location, foodpref, wc, activity, history = "Yogesh",23, "male", "India", 3, 70, 3, 2
+    results = {
+        "Patient Name" : name,
+        "Age": age,
+        "Sex": sex,
+        "Location" : location,
+        "Food Preference" : foodpref,
+        "Waist Circumference": wc,
+        "Activity Level": 2,
+        "Diabetes History": 2,
+        "Total Score": 30,
+        "Prediction": "high risk"
+    }
+    return results
 
 @app.route('/about')
 def about():
